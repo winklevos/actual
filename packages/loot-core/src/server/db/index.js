28d2c182907322e45caf91a2fd8d614d51ae6321
Tom French <tom@tomfren.ch>
2022-06-01 01:20:13 +0100

    refactor: move loadClock into `clock` file

--- packages/loot-core/src/server/db/index.js
+++ packages/loot-core/src/server/db/index.js
@@ -1,35 +1,9 @@
-import Timestamp, {
-  makeClock,
-  setClock,
-  serializeClock,
-  deserializeClock,
-  makeClientId
-} from '../timestamp';
-import { first, runQuery } from './db-actions';
-
 export { toDateRepr, fromDateRepr } from '../models';
 export * from "./db-actions"
 export * from "./db-connection"
+export { loadClock } from "./clock";
 export * from "./accounts";
 export * from "./categories";
 export * from "./payees";
 export * from "./transactions";
 
-export async function loadClock() {
-  let row = await first('SELECT * FROM messages_clock');
-  if (row) {
-    let clock = deserializeClock(row.clock);
-    setClock(clock);
-  } else {
-    // No clock exists yet (first run of the app), so create a default
-    // one.
-    let timestamp = new Timestamp(0, 0, makeClientId());
-    let clock = makeClock(timestamp);
-    setClock(clock);
-
-    await runQuery('INSERT INTO messages_clock (id, clock) VALUES (?, ?)', [
-      1,
-      serializeClock(clock)
-    ]);
-  }
-}
