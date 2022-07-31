bce8e1778656bc86eef1dc7c5e7b6cc738de1de4
winklevos <winklevos@outlook.com>
2022-07-31 12:35:09 +1000

    da

---
+++ development-getting-started.md
@@ -0,0 +1,10 @@
+*Requirements*
+
+1. Run `choco install nodejs`
+2. Run `corepack enable` 
+3. Run `choco install python`
+3. Run `choco install visualstudio2022buildtools`
+4. Run `choco install visualstudio2022-workload-node`
+4. Run `choco install visualstudio2022-workload-vctools`
+4. Run `npm config set msvs_version 2022`
+3. Run `yarn install`
---
+++ windows-dev-choco.ps1
@@ -0,0 +1,19 @@
+# choco install wsl2 -y
+wsl --install
+# choco install wsl-ubuntu-2004 -y
+choco install nodejs -y
+choco install python --version 3.9.13 -y
+choco install python2 -y
+choco install yarn -y
+# choco install visualstudio2022buildtools -y
+choco install visualstudio2019buildtools -y
+# choco install visualstudio2022-workload-nativedesktop -y
+choco install visualstudio2019-workload-nativedesktop -y
+# choco install visualstudio2022-workload-node -y
+choco install visualstudio2019-workload-node -y
+# choco install visualstudio2022-workload-vctools -y
+choco install visualstudio2019-workload-vctools -y
+
+
+
+# npm config set msvs_version 2022
